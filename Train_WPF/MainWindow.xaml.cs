ddc9a034e663e36a4f0e32fa93e949a72d7d6d25
Alex Caron <alecaron@hotmail.fr>
2021-04-14 22:08:02 +0200

    Ajouter des commentaires pour le drag'n'drop

--- Train_WPF/MainWindow.xaml.cs
+++ Train_WPF/MainWindow.xaml.cs
@@ -16,9 +16,6 @@ using Train_Models;
 
 namespace Train_WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private ICollection<Expedition> expeditions = new List<Expedition>();
@@ -31,8 +28,12 @@ namespace Train_WPF
             Area_OilWell.Tag = new Lieu("OilWell", ressource.gas, 2, 2, 2);
         }
 
+        //Pour drag un personnage
         private void CharacterMouseMove(object sender, MouseEventArgs e)
         {
+            //On recupère l'objet qu'on drag et on le rentre dans la variable
+            //On vérifie qu'on clique et qu'on a bien une image avec un tag personnage, alors on autorise le drag
+
             Image image = sender as Image;
             if (image != null && e.LeftButton == MouseButtonState.Pressed && image.Tag !=null && image.Tag is Personnage tag)
             {
@@ -40,14 +41,23 @@ namespace Train_WPF
             }
         }
 
+        //Lorsqu'on passe sur le lieu pour drop personnage
         private void AreaDragEnter(object sender, DragEventArgs e)
         {
             //
 
         }
 
+        //Lorsqu'on drop un personnage
         private void AreaDrop(object sender, DragEventArgs e)
         {
+            //Si on a bien une image avec un tag lieu
+            //On recupere les data de l'objet qu'on drop
+            //On cherche si il existe une expedition avec le même lieu que l'object sender dans la liste "expeditions"
+            //Si elle n'existe pas on en créer une et on l'ajoute à la liste "expeditions"
+            //On cherche si il y'a déjà un perso dans l'expedition avec le même nom que celui qu'on drop
+            //Si ce n'est pas le cas on l'ajoute au groupe de l'expedition
+
             if (sender is Image image && image.Tag != null && image.Tag is Lieu lieu)
             {
                 Personnage personnage = e.Data.GetData(typeof(Personnage)) as Personnage;
